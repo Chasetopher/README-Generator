c5c4b8374571b7dc80a46b85a827851ecd71394d
Chasetopher <85038033+Chasetopher@users.noreply.github.com>
2021-08-15 21:49:20 -0400

    removed broken functions from generate markdown, changed init function, added writeToFile function back

--- index.js
+++ index.js
@@ -3,54 +3,53 @@ const fs = require('fs');
 const generateMarkdown = require('./utils/generateMarkdown');
 
 
-const questions = () => {
-  return inquirer.prompt([
+const questions = [
       //prompt for user to enter their github username and email
       {
         type: 'input',
         name: 'github',
-        message: 'Enter your GitHub Username: '
+        message: 'Enter your GitHub Username:'
       },
       {
         type: 'input',
         name: 'email',
-        message: 'Enter your Email Address: '
+        message: 'Enter your Email Address:'
       },
       //prompt for user to input details and information about their project
       {
         type: 'input',
         name: 'link',
-        message: 'Enter the GitHub link to your project: '
+        message: 'Enter the GitHub link to your project:'
       },
       {
         type: 'input',
         name: 'title',
-        message: 'Enter the name of your project: '
+        message: 'Enter the name of your project:'
       },
       {
         type: 'input',
         name: 'description',
-        message: 'Provide a description of the project: '
+        message: 'Provide a description of the project:'
       },
       {
         type: 'input',
         name: 'installation',
-        message: 'Provide installation instructions for the project: '
+        message: 'Provide installation instructions for the project:'
       },
       {
         type: 'input',
         name: 'usage',
-        message: 'Provide instructions and examples for use: '
+        message: 'Provide instructions and examples for use:'
       },
       {
         type: 'input',
         name: 'contribute',
-        message: 'Enter guidelines for contributions: '
+        message: 'Enter guidelines for contributions:'
       },
       {
         type: 'input',
         name: 'test',
-        message: 'Provide test instructions for the project: '
+        message: 'Provide test instructions for the project:'
       },
       {
         type: 'list',
@@ -58,25 +57,30 @@ const questions = () => {
         message: 'Select your project License',
         choices: ['No License', 'GPL-3.0', 'MPL-2.0', 'Apache-2.0', 'MIT']
       }
-    ]);
-};
+    ];
 
-// TODO: Create a function to write README file
+    // TODO: Create a function to write README file
+function writeToFile(fileName, data){
+  fs.writeFile('./dist/README.md',(fileName, data), err => {
+      if (err){
+          console.log(err);
+          return
+      } else {
+      console.log (`
+      ===============================
+      README.md successfully created!
+      ===============================
+      `);
+      }
+  })
+}
+
+// TODO: Create a function to initialize app
 function init() {
-  questions()
-    .then(data => {
-      fs.writeFile('./dist/README.md', generateMarkdown(data), err => {
-        if (err) {
-          throw err
-        } else {
-          console.log(`
-          ===============================
-          README.md successfully created!
-          ===============================
-          `)
-        }
+      return inquirer.prompt(questions)
+      .then((data)=>{
+          writeToFile('README.md', generateMarkdown(data))
       })
-    })
 }
 
 // Function call to initialize app
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,15 +1,15 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-    if (license == 'No License') {
+    if (license === 'No License') {
         return ''
-    } else if (license == 'GPL-3.0') {
+    } else if (license === 'GPL-3.0') {
         return '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)'
-    } else if (license == 'MPL-2.0') {
+    } else if (license === 'MPL-2.0') {
         return '[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)'
-    } else if (license == 'Apache-2.0') {
+    } else if (license === 'Apache-2.0') {
         return '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)'
-    } else if (license == 'MIT') {
+    } else if (license === 'MIT') {
         return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)'
     }
 }
@@ -17,38 +17,19 @@ function renderLicenseBadge(license) {
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
-    if (license == 'No License') {
+    if (license === '') {
         return ''
     } else {
         return 'https://choosealicense.com/licenses/${data.license.toLowerCase()}/'
     }
 }
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {
-    if (license == 'No License') {
-        return ''
-    } else {
-        return `
-        ## License
-
-        ### ${data.license} ${renderLicenseBadge(data.license)}
-
-        For more information about this license please visit ${renderLicenseLink(data.license)}.
-
-        `
-    }
-}
-
 //if there is a license, generate a link in TOC
 function renderlicenseTOC(license) {
-    if (license == 'No License') {
+    if (license === 'No License') {
         return ''
     } else {
-        return `
-        *[License](#license)
-        `
+        return `* [License](#license)`
     }
 }
 
@@ -69,7 +50,7 @@ function generateMarkdown(data) {
     * [Features](#features)
     * [Tests](#tests)
     * [Questions](#questions)
-    ${renderlicenseTOC(data.license)}
+    ${renderlicenseTOC()}
 
     ## Installation
 
@@ -95,9 +76,12 @@ function generateMarkdown(data) {
 
     Email: ${data.email}
 
-    ${renderLicenseSection(data.license)}
+    ## License
 
-    
+    ### ${data.license} ${renderLicenseBadge(data.license)}
+
+    For more information about this license please visit ${renderLicenseLink(data.license)}.
+
 `;
 }
 
